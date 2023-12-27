25573335729631cb32d842a9e2aedd27163d1200
Mailsss <150383693+Mailsss@users.noreply.github.com>
2023-12-27 19:44:45 +0300

    Add files via upload

---
+++ trampl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpTrigger : MonoBehaviour
+{
+    public float jumpFactor = 2.0f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //���������� �������� ���� ������
+        other.GetComponent<Jump>().jumpStrength *= jumpFactor;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //��������� �������� ���� ������
+        other.GetComponent<Jump>(). jumpStrength /= jumpFactor;
+    }
+}
